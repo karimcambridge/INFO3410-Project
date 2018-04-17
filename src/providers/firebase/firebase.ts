a9f98486a2ee04b22bf67c4350ef2e2f915d646d
rastin10 <37004111+rastin10@users.noreply.github.com>
2018-04-17 18:50:37 -0300

    Providers update

--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { AngularFireDatabase } from 'angularfire2/database';
 /*
   Generated class for the FirebaseProvider provider.
 
@@ -10,8 +10,20 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class FirebaseProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello FirebaseProvider Provider');
+  constructor(public afd: AngularFireDatabase) {}
+
+  getShoppingItems() {
+    return this.afd.list('/shoppingItems/');
   }
+ 
+  addItem(name) {
+    this.afd.list('/shoppingItems/').push(name);
+  }
+ 
+  removeItem(id) {
+    this.afd.list('/shoppingItems/').remove(id);
+  }
+
+ 
 
 }
