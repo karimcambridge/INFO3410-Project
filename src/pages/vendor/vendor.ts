cc933b2eaa0fc71f9fc80fd16801d30e1af27608
zariagrant <37633897+zariagrant@users.noreply.github.com>
2018-04-17 17:42:26 -0400

    created vendor tab

---
+++ src/pages/vendor/vendor.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-vendor',
+  templateUrl: 'vendor.html'
+})
+export class VendorPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
