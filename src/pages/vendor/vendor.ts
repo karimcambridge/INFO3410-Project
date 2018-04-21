c735b1b21ad50b9ff585a07288400db71b3ef2c0
zariagrant <37633897+zariagrant@users.noreply.github.com>
2018-04-20 21:21:25 -0400

    smtg

--- src/pages/vendor/vendor.ts
+++ src/pages/vendor/vendor.ts
@@ -4,7 +4,9 @@ import { NavController } from 'ionic-angular';
 @Component({
   selector: 'page-vendor',
   templateUrl: 'vendor.html'
+  //template: '<ion-nav [root]="rootPage"></ion-nav>'
 })
+
 export class VendorPage {
 
   constructor(public navCtrl: NavController) {
