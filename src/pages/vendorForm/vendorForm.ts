712ce348160bffd52fc01f4e7a69bf0a845b5aa4
sadeb96 <37006407+sadeb96@users.noreply.github.com>
2018-04-21 13:38:39 -0400

    create vendor form

---
+++ src/pages/vendorForm/vendorForm.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-vedorForm',
+  templateUrl: 'vendorForm.html'
+})
+export class venforFormPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
