16b7704bb8fc3275fb8ac52385eb0d7c7e79bb58
zariagrant <37633897+zariagrant@users.noreply.github.com>
2018-04-21 13:25:56 -0400

    got rid of sign in page

--- src/pages/sign-in/sign-in.module.ts
+++ src/pages/sign-in/sign-in.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { SignInPage } from './sign-in';
+// import { NgModule } from '@angular/core';
+// import { IonicPageModule } from 'ionic-angular';
+// import { SignInPage } from './sign-in';
 
-@NgModule({
-  declarations: [
-    SignInPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(SignInPage),
-  ],
-})
-export class SignInPageModule {}
+// @NgModule({
+//   declarations: [
+//     SignInPage,
+//   ],
+//   imports: [
+//     IonicPageModule.forChild(SignInPage),
+//   ],
+// })
+// export class SignInPageModule {}
