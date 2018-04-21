e15d40dfd47f10ef4ef95e31a6c93777e6bcb0e2
sadeb96 <37006407+sadeb96@users.noreply.github.com>
2018-04-21 13:54:24 -0400

    vendor

---
+++ src/pages/listVendors/listVendors.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+// import {listVendor.html} '../pages/vendor/vendor';
+@Component({
+  selector: 'page-listVendor',
+  templateUrl: 'listVendor.html'
+})
+export class ListVendor {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
