259161ebf51e25cf00b6c2d4971e7474f4f09bca
zariagrant <37633897+zariagrant@users.noreply.github.com>
2018-04-21 17:19:02 -0400

    add login page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { FirebaseProvider } from './../../providers/firebase/firebase';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { GoogleLoginComponent } from '../../components/google-login/google-login';
 
 @Component({
   selector: 'page-home',
@@ -10,6 +11,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  signIn() {
+    this.navCtrl.setRoot(GoogleLoginComponent);
   }
 
 }
