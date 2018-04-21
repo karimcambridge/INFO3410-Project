dbda21c62e2eaa19ce12de4c574337bac2b273be
sadeb96 <37006407+sadeb96@users.noreply.github.com>
2018-04-21 19:34:18 -0400

    google maps attempt
    https://www.joshmorony.com/ionic-2-how-to-use-google-maps-geolocation-video-tutorial/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import {VendorFormPage} from '../pages/vendorForm/vendorForm';
 import {ListVendorPage} from '../pages/listVendors/listVendors';
 import {ModalContentPage} from '../pages/listVendors/modal-content';
 
+import { Geolocation } from '@ionic-native/geolocation';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -95,6 +96,7 @@ export class MyErrorHandler implements ErrorHandler {
   providers: [
     StatusBar,
     SplashScreen,
+    Geolocation,
     FirebaseProvider,
     GooglePlus,
     Calendar,
