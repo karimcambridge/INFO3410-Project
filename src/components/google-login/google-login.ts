1cd7a1fa85e88e9706b5c6ab1b0114460bdc4057
Kar <karimcambridge@gmail.com>
2018-04-21 15:39:11 -0400

    Fixed last commit

--- src/components/google-login/google-login.ts
+++ src/components/google-login/google-login.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+
+import { GooglePlus } from '@ionic-native/google-plus';
+import { Platform } from 'ionic-angular';
+
 /**
  * Generated class for the GoogleLoginComponent component.
  *
@@ -19,43 +26,48 @@ export class GoogleLoginComponent {
               private platform: Platform) {
 
     this.user = this.afAuth.authState;
-}
 
-async nativeGoogleLogin(): Promise<void> {
-  try {
-
-    const gplusUser = await this.gplus.login({
-      'webClientId': '833083649643-snd72roi89uhcsc47me8l96j8tocamkk.apps.googleusercontent.com',
-      'offline': true,
-      'scopes': 'profile email'
-    })
-
-    return await this.afAuth.auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(gplusUser.idToken))
-
-  } catch(err) {
-    console.log(err)
-  }
-}
-
-async webGoogleLogin(): Promise<void> {
-  try {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    const credential = await this.afAuth.auth.signInWithPopup(provider);
-
-  } catch(err) {
-    console.log(err)
   }
 
-}
+  /// Our login Methods will go here
 
-googleLogin() {
-  if (this.platform.is('cordova')) {
-    this.nativeGoogleLogin();
-  } else {
-    this.webGoogleLogin();
+  async nativeGoogleLogin(): Promise<void> {
+    try {
+
+      const gplusUser = await this.gplus.login({
+        'webClientId': '833083649643-snd72roi89uhcsc47me8l96j8tocamkk.apps.googleusercontent.com',
+        'offline': true,
+        'scopes': 'profile email'
+      })
+
+      return await this.afAuth.auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(gplusUser.idToken))
+
+    } catch(err) {
+      console.log(err)
+    }
   }
-}
 
-signOut() {
-  this.afAuth.auth.signOut();
+  async webGoogleLogin(): Promise<void> {
+    try {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      const credential = await this.afAuth.auth.signInWithPopup(provider);
+
+    } catch(err) {
+      console.log(err)
+    }
+
+  }
+
+  googleLogin() {
+    if (this.platform.is('cordova')) {
+      this.nativeGoogleLogin();
+    } else {
+      this.webGoogleLogin();
+    }
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
+  }
+
 }
