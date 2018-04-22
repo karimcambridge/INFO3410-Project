f9701ad4bec62a5fee18e431a33cf6488366d336
Kar <karimcambridge@gmail.com>
2018-04-22 15:27:22 -0400

    Add auth service, services folder and login function

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -31,8 +31,6 @@ export class HomePage {
 
     this.user = this.afAuth.authState;
   }
-<<<<<<< HEAD
-=======
   
 	get authenticated(): boolean {
 	  return this.user !== null;
@@ -53,7 +51,6 @@ export class HomePage {
       console.log(err)
     }
   }
->>>>>>> 4fdecb382510c034a5acedb6330e0288b563750f
 
   async webGoogleLogin(): Promise<void> {
     try {
@@ -131,25 +128,5 @@ export class HomePage {
     });
    
   }
-  
-
-  /// Our login Methods will go here
-
-  async nativeGoogleLogin(): Promise<void> {
-    try {
-
-      const gplusUser = await this.gplus.login({
-        'webClientId': '833083649643-snd72roi89uhcsc47me8l96j8tocamkk.apps.googleusercontent.com',
-        'offline': true,
-        'scopes': 'profile email'
-      })
-
-      return await this.afAuth.auth.signInWithCredential(firebase.auth.GoogleAuthProvider.credential(gplusUser.idToken))
-
-    } catch(err) {
-      console.log(err)
-    }
-  }
-
 
 }
