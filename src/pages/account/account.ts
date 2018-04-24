81f4e5909b0a4f6dd88ab77a4c02c4ddcf8d5430
Kar <karimcambridge@gmail.com>
2018-04-24 15:36:34 -0400

    Add logged in check to account page

--- src/pages/account/account.ts
+++ src/pages/account/account.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireAuth } from 'angularfire2/auth';
 import { AuthService } from '../../app/core/auth.service';
 import { LoginPage } from '../login/login';
 import { HomePage } from '../home/home';
@@ -10,10 +11,15 @@ import { HomePage } from '../home/home';
   templateUrl: 'account.html',
 })
 export class AccountPage {
+  isLoggedIn: boolean = false;
 
   constructor(public navCtrl: NavController,
-  			public navParams: NavParams,
-  			public authService: AuthService) {
+        public navParams: NavParams,
+        private afAuth: AngularFireAuth,
+        public authService: AuthService) {
+    this.afAuth.authState.subscribe(res => {
+      this.isLoggedIn = (res && res.uid) ? true : false;
+    });
   }
 
   gotoLoginPage() {
