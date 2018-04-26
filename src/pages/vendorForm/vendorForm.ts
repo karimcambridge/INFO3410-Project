2302adf15499e18ebc456b464de156c9414ce548
Kar <karimcambridge@gmail.com>
2018-04-26 17:26:58 -0400

    Improve vendor sign up form

--- src/pages/vendorForm/vendorForm.ts
+++ src/pages/vendorForm/vendorForm.ts
@@ -18,8 +18,8 @@ export class VendorFormPage {
     this.vendorForm = fb.group({
       tel: ['', Validators.compose([Validators.required, Validators.minLength(7)])],
       category: ['', Validators.compose([Validators.required])],
-      quote: ['', Validators.compose([Validators.required]), Validators.minLength(4)],
-      photo_url: ['', Validators.compose([Validators.required])]
+      quote: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      photo_url: ['', Validators.compose([Validators.required, Validators.minLength(6)])]
     });
   }
 
